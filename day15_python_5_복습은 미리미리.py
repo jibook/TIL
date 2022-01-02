d2b255dde0eddae08cf906dd08a92595d89db3df
jihyun <jhjh306@gmail.com>
2022-01-02 16:39:25 +0900

    2022 first commit

---
+++ day15_python_5_복습은 미리미리.py
@@ -0,0 +1,181 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 29 22:07:16 2021
+
+@author: jhjh3
+"""
+
+run my_modules
+
+df1= DataFrame(np.arange(1,17).reshape(4,4))
+df1
+dir(df1)
+
+df1.sum(axis=0)
+df1.sum(axis=1)
+
+df1.iloc[:,0]
+df1.iloc[:,0].sum()
+df1.iloc[:,0].mean()
+
+
+df1.iloc[0,0] = np.nan
+df1
+'''
+      0   1   2   3
+0   NaN   2   3   4
+1   5.0   6   7   8
+2   9.0  10  11  12
+3  13.0  14  15  16
+'''
+df1.iloc[:,0].mean()   # 9 없는건 무시하고 연산
+
+# 평균값(최대 또는 최소) 대치
+df1.iloc[:,0].mean()
+df1.iloc[:,0].isnull()   # 조건(boolean)
+df1
+df1.iloc[:,0][df1.iloc[:,0].isnull()]=df1.iloc[:,0].mean()
+df1
+'''
+      0   1   2   3
+0   9.0   2   3   4
+1   5.0   6   7   8
+2   9.0  10  11  12
+3  13.0  14  15  16
+'''
+df1.isnull()
+df1[df1.notnull()] 
+df1[df1.isnull()]  # 데이터프레임 전체에서 NaN 값 확인
+#     0   1   2   3
+# 0 NaN NaN NaN NaN
+# 1 NaN NaN NaN NaN
+# 2 NaN NaN NaN NaN
+# 3 NaN NaN NaN NaN
+
+df1.iloc[:,0].var()   # 10.66666666666666 >> 분산
+df1.iloc[:,0].std()   # 3.265986323710904 >> 표준편차
+df1.iloc[:,0].min()   # 5.0 >> 최솟값
+df1.iloc[:,0].max()   # 13.0 >> 최댓값
+df1.iloc[:,0].median() # 9.0 >> 중위수(중앙값)
+
+round(df1.iloc[:,0].var(),2)
+
+
+
+
+
+
+# replace 매서드_치환, 삭제
+
+# replace 매서드
+# 치환(찾을 문자열, 바꿀 문자열)
+
+# 1. 기본 문자열 매서드
+# - 기본 파이썬 제공
+# - 문자열에서 호출 가능
+# - 백터 연산(각 원소별 반복 처리 불가)
+# - 오직 문자열 치환만 가능(숫자치환, NA 치환 불가능)
+
+'abcd'.replace('a', 'A') # 'Abcd' >> a를 A로 치환
+'abcd'.replace('a', '')  # 'abcd' >> 공백으로 치환
+# 'abcd'.replace(1, '')
+# TypeError: replace() argument 1 must be str, not int >> 숫자는 치환 안된다
+# ' bcd'.replace('',1)
+# TypeError: replace() argument 2 must be str, not int >> 숫자로 치환하는거안된다
+# ['abcd','abcde','aabb'].replace(',','')
+
+
+# for 문 활용
+outlist=[]
+for i in ['abcd','abcde','aabb']:
+    outlist.append(i.replace('a','A'))
+print(outlist)
+# ['Abcd', 'Abcde', 'AAbb']
+
+# map 활용
+f1 = lambda x : x.replace('a','A')
+list(map(f1,['abcd','abcde','aabb']))
+# ['Abcd', 'Abcde', 'AAbb']
+
+['abcd','abcde','aabb'].map(f1)
+fff=Series(['abcd','abcde','aabb'])
+fff.map(f1)
+# Series는 map을 할 수 있고, list는 map을 할 수 없다.
+
+Series(['abcd','abcde','aaaab',np.nan]).map (lambda x: x.replace(np.nan,''))
+# TypeError: replace() argument 1 must be str, not float
+# 문자열(str)이여야 함, NaN이 있으면 안돼
+# 0     abcd
+# 1    abcde
+# 2    aaaab
+# 3      NaN
+# dtype: object
+
+
+
+
+# 2. str.replace
+# - pandas 제공 (Series 객체만 호출 가능)
+# - 백터화 내장된 문자열 매서드
+# - 문자열 호출 가능
+# - 백터 연산(각 원소별 반복 처리) 가능
+# - 오직 문자열 치환만 가능(숫자 치환, NA 치환 불가)
+# - 숫자로 구성된 Series 적용 불가
+
+Series(['abcd','abcde','aaaab']).str.replace('a', 'A')
+# list, DataFrame 안되고 Series만 된다
+
+
+# pandas replace
+# - pandas 제공
+# - 값 치환 메서드(똑같은 , 완전히 일치하는 값을 다른 값으로 대체, 삭제 )
+# - Series, DataFrame 호출 가능 
+# - 숫자, NaN 치환 가능 
+# - 동시에 여러 대상 수정 가능
+
+
+df1 = DataFrame([['1,200','1,300'],['1,400','1,500']])
+df1
+df1.replace(',','')  # 변화 없음 ','로 생긴 값을 삭제하는 의미
+
+df2 = DataFrame([['1,200',','],['1,400','1,500']])
+df2
+df2.replace(',','')
+
+df3 = DataFrame([['1200','1300'],['1400','.']], columns =['a','b'])
+df3
+df3.replace('.', np.nan)  # '.' 와 완전히 일치하는 값은 NaN 치환
+df3.replace(['.','1400','?','!'], np.nan) 
+#       a     b
+# 0  1200  1300
+# 1   NaN   NaN
+
+
+
+# [연습 문제]
+# df1에 천단위 구분기호 제거 후 모두 숫자 변경
+df1
+# edf1= lambda x : x.find(',')
+# list(map())
+# df1.DataFrame(find(','))     틀린거 연습해봐 더 
+edf1=lambda x: x.find(',')
+
+df1.applymap(lambda x : int(x.replace(',','')))
+
+
+df1.applymap(lambda x : int(x.replace(',',''))).sum()
+
+# 동료 코드1
+copy_df1=df1
+for i in range(0,len(df1)):
+    copy_df1[i]=df1[i].str.replace(',','')
+print(copy_df1)
+
+# 동료 코드2
+df1 = DataFrame([['1,200','1,300'],['1,400','1,500']])
+for i in range(len(df1)):
+    if df1[i].all() != 'int':
+        df1[i] = df1[i].str.replace(',','').astype('int')
+    else:
+        pass
+print(df1)
---
+++ day15_python_예제 및 알아야할 것.md
@@ -0,0 +1,143 @@
+#### DataFrame
+
+DataFrame(np.arange(1,7).reshape(2,3), index=['a','b'], columns=['col1','col2','col3'])
+```   A  B  C
+a  1  2  3
+b  4  5  6
+```
+
+np.arange(1,7).reshape(2,3)
+
+```array([[1, 2, 3],
+array([1,2,3],[4, 5, 6])
+```
+
+
+
+#### iloc, loc ****
+
+- iloc : positional indexing 숫자로 위치를 뽑아옴[행인덱스,열인덱스]
+- loc(location) : label indexing 해당하는 행을 뽑아온다
+
+
+
+#### 예제 - 이메일 아이디 추출 / 맵람다형태확인
+
+s6=Series(['abc@abc.com','avcvdds@abc.com'])
+
+1. s6.map(lambda x: x[:x.find('@')])
+2. s6.map(lambda x: x[:x.find('@')])
+3. list(s6.map(lambda x: x[:x.find('@')]))
+4. s6id=s6.str.find('@')
+   list(map(lambda x,y: x[0:y],s6,s6id))
+
+
+
+#### 문자열 제거(제거함수 : 공백, 문자)
+
+Series(map(lambda x: x.replace('a',''),['aaabaaca','abcba']))
+Series(['aaabaaca','abcba']).str.replace('a','')
+
+Series(['abcd','abcde','aaaab']).str.replace('a', 'A')
+
+
+
+#### [예제] 천단위 구분기호 처리
+
+s3=Series(['1,200','3,000','4,000'])
+s3.str.replace(',','')
+
+``` 0    1200
+1    3000
+2    4000
+dtype: object
+0    1200
+1    3000
+2    4000
+dtype: object
+```
+
+s3.str.replace(',','').astype(int)              # 객체에서 숫자로 바꿔줘야 합할 수 있지
+s3.str.replace(',','').astype(int).sum()   # 더했다! 8200
+
+
+
+#### df1.sum(axis=0)    열
+#### df1.sum(axis=1)    행
+
+
+
+
+
+#### [예제] df1에 천단위 구분기호 제거 후 모두 숫자 변경
+
+df1 = DataFrame([['1,200','1,300'],['1,400','1,500']])
+
+edf1= lambda x : x.find(',')
+
+list(map())
+
+df1.DataFrame(find(','))    
+
+
+
+
+df1.applymap(lambda x : int(x.replace(',',''))).sum()
+
+
+
+##### 동료 코드1
+
+```python
+copy_df1=df1
+for i in range(0,len(df1)):
+    copy_df1[i]=df1[i].str.replace(',','')
+print(copy_df1)
+
+#      0     1
+#0  1200  1300
+#1  1400  1500
+```
+
+  0     1
+
+0  1200  1300
+1  1400  1500
+
+##### 동료 코드2
+
+```python
+df1 = DataFrame([['1,200','1,300'],['1,400','1,500']])
+for i in range(len(df1)):
+    if df1[i].all() != 'int':
+        df1[i] = df1[i].str.replace(',','').astype('int')
+    else:
+        pass
+print(df1)
+
+#      0     1
+#0  1200  1300
+#1  1400  1500
+```
+
+
+
+#### '.' 확인 시 NaN으로 변경
+
+```
+import numpy as np
+
+df1['a'][df1['a'] == '.'] = np.nan  # 점발견
+```
+
+3    .
+
+Name: a, dtype: object
+
+
+
+#### train/test 로 나눠서 데이터 분석(머신러닝에선 필쑤!!)
+
+- 학습전에 스케일링을 시키고 해야함
+
+train_x, test_x, train_y, test_y = train_test_split(iris_x, iris_y)
