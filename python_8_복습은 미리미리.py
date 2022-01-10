40ff80306900e283477b5faeb3cae0f37a3e1f09
jihyun <jhjh306@gmail.com>
2022-01-10 20:44:12 +0900

    2022 9th commit update

--- python_8_복습은 미리미리.py
+++ python_8_복습은 미리미리.py
@@ -29,7 +29,7 @@ Created on Sat Jan  8 22:23:16 2022
 # 2. unstack
 # long --> wide
 
-run my_modules
+# run my_modules
 
 kimchi = pd.read_csv("./data/kimchi.csv",encoding='cp949')
 
@@ -45,6 +45,46 @@ df1 = kimchi.groupby(['판매년도','제품'])['수량'].sum()
 df2=df1.unstack()    # long --> wide 행이 컬럼이 바뀐다 대각선을 기준으로 
 df2.stack()   
 
+df1.unstack(level=0) # 상위레벨(2013) : 행세 인덱스가 컬럼으로 간다
+df1.unstack(level=1) 
+
+df2.stack(level=0)  # 얜 똑같네
+
+
+# pivot_table
+# 교차표 작성
+
+kimchi.pivot_table(index = '판매월',      # index 방향에 배치할 컬럼명
+                   columns ='판매처',     # column 방향에 배치할 컬럼명
+                   values ='수량',        # 교차표에 작성할 값을 갖는 컬럼명
+                   aggfunc ='sum'         # 그룹 함수
+                   )
+
+
+# 예제 kimchi 데이터를 이용해서 연도별, 제품별, 판매금액의 총합을 교차표로 작성하세요
+kimchi
+
+kimchi.pivot_table(index = '판매년도', columns = "제품", values = "판매금액", aggfunc = "sum")
+'''
+제품           무김치        열무김치        총각김치
+판매년도                                    
+2013  3809133886  3416217894  4186789117
+2014  4111403907  4392490590  5250364301
+2015  5212738410  5859079408  7485159996
+2016  6903506142  6627888927  6378374872
+'''
+
+
+
+# ------------------------------------------NA 결측치 처리, 중복값 제거 (왕중요)
+
+# NA (결측치) 처리 
+# 숫자형 NA (float type), 문자형 NA
+
+
+# run my_modules
+
+s1 = Series([1,2,3, np.nan])
 
 
 
